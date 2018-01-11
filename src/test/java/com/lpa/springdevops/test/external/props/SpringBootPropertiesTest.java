c8769a29294f594095e5272254fb6bb76475403a
L.P <parisot.laurent@gmail.com>
2018-01-11 12:28:58 +0100

    Spring boot application properties

---
+++ src/test/java/com/lpa/springdevops/test/external/props/SpringBootPropertiesTest.java
@@ -0,0 +1,28 @@
+package com.lpa.springdevops.test.external.props;
+
+import com.lpa.springdevops.test.jms.FakeJmsBroker;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@TestPropertySource("/application.properties")
+@SpringBootTest
+public class SpringBootPropertiesTest {
+    @Autowired
+    FakeJmsBroker fakeJmsBroker;
+
+    @Test
+    public void testPropsSet() throws Exception {
+        assertEquals("10.10.10.123", fakeJmsBroker.getUrl());
+        assertEquals(3330, fakeJmsBroker.getPort().intValue());
+        assertEquals("Ron", fakeJmsBroker.getUser());
+        assertEquals("Burgundy", fakeJmsBroker.getPassword());
+    }
+
+}
