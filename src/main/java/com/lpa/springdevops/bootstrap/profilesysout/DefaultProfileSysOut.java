be6e3fb35bb552f677e88502954986d64d960934
L.P <parisot.laurent@gmail.com>
2018-01-15 11:33:02 +0100

    Active profile at runtime

---
+++ src/main/java/com/lpa/springdevops/bootstrap/profilesysout/DefaultProfileSysOut.java
@@ -0,0 +1,17 @@
+package com.lpa.springdevops.bootstrap.profilesysout;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@Component
+@Profile("default")
+public class DefaultProfileSysOut {
+
+    public DefaultProfileSysOut() {
+        System.out.println("##################################");
+        System.out.println("##################################");
+        System.out.println("##            DEFAULT           ##");
+        System.out.println("##################################");
+        System.out.println("##################################");
+    }
+}
---
+++ src/main/java/com/lpa/springdevops/bootstrap/profilesysout/ProdProfileSysOut.java
@@ -0,0 +1,16 @@
+package com.lpa.springdevops.bootstrap.profilesysout;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@Component
+@Profile("prod")
+public class ProdProfileSysOut {
+    public ProdProfileSysOut() {
+        System.out.println("##################################");
+        System.out.println("##################################");
+        System.out.println("##              PROD            ##");
+        System.out.println("##################################");
+        System.out.println("##################################");
+    }
+}
