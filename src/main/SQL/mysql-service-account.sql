4e3dfac25ab653b816963461af8c0eb37e2005de
L.P <parisot.laurent@gmail.com>
2018-01-25 12:36:20 +0100

    Add a new profile named qa2 using a new database with qa2user

--- src/main/SQL/mysql-service-account.sql
+++ src/main/SQL/mysql-service-account.sql
@@ -4,3 +4,12 @@ GRANT SELECT ON springguru.* to 'springframework'@'localhost';
 GRANT INSERT ON springguru.* to 'springframework'@'localhost';
 GRANT DELETE ON springguru.* to 'springframework'@'localhost';
 GRANT UPDATE ON springguru.* to 'springframework'@'localhost';
+
+CREATE DATABASE qa2;
+
+CREATE USER 'qa2user'@'%' IDENTIFIED BY 'qa2password';
+
+GRANT SELECT ON qa2.* to 'qa2user'@'%';
+GRANT INSERT ON qa2.* to 'qa2user'@'%';
+GRANT DELETE ON qa2.* to 'qa2user'@'%';
+GRANT UPDATE ON qa2.* to 'qa2user'@'%';
